9f885391d29aa8adba76c542fc8492ee4a235675
Vladislav Dykhnenko <vlad43452764@gmail.com>
2022-07-03 19:39:41 +0300

    тест пула данных

--- src/assets/js/app.js
+++
@@ -1,9 +0,0 @@
-import Alpine from 'alpinejs'
-import lozad from 'lozad'
-
-window.Alpine = Alpine
-
-Alpine.start()
-
-const observer = lozad();
-observer.observe();
--- utils/passthroughs.js
+++
@@ -1,6 +0,0 @@
-
-module.exports = [
-    'src/CNAME', // Needed by GitHub Pages
-    'src/images',
-    'src/apispecs' // For apibrowser
-  ]
